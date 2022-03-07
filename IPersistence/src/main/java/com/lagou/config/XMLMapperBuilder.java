53971bbd84e60b44b06d326705b801aa3b73d713
Mason <ch_gjm@163.com>
2022-03-07 20:01:32 +0800

    Add：IPersistence

---
+++ IPersistence/src/main/java/com/lagou/config/XMLMapperBuilder.java
@@ -0,0 +1,47 @@
+package com.lagou.config;
+
+import com.lagou.pojo.Configuration;
+import com.lagou.pojo.MappedStatement;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class XMLMapperBuilder {
+
+    private Configuration configuration;
+
+    public XMLMapperBuilder(Configuration configuration) {
+        this.configuration =configuration;
+    }
+
+    public void parse(InputStream inputStream) throws DocumentException {
+
+        Document document = new SAXReader().read(inputStream);
+        Element rootElement = document.getRootElement();
+
+        String namespace = rootElement.attributeValue("namespace");
+
+        List<Element> list = rootElement.selectNodes("//select");
+        for (Element element : list) {
+            String id = element.attributeValue("id");
+            String resultType = element.attributeValue("resultType");
+            String paramterType = element.attributeValue("paramterType");
+            String sqlText = element.getTextTrim();
+            MappedStatement mappedStatement = new MappedStatement();
+            mappedStatement.setId(id);
+            mappedStatement.setResultType(resultType);
+            mappedStatement.setParamterType(paramterType);
+            mappedStatement.setSql(sqlText);
+            String key = namespace+"."+id;
+            configuration.getMappedStatementMap().put(key,mappedStatement);
+
+        }
+
+    }
+
+
+}
